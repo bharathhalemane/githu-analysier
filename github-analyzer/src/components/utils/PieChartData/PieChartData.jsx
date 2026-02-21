dfc014d9fc5aa54431ef5151cb6ad670065c55d1
bharath <bharathhalemane143@gmail.com>
2026-02-21 19:47:27 +0530

    msg

--- github-analyzer/src/components/utils/PieChartData/PieChartData.jsx
+++ github-analyzer/src/components/utils/PieChartData/PieChartData.jsx
@@ -3,26 +3,26 @@ import './PieChartData.css'
 
 const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"]
 
-const PieChartData = ({raw}) => {
-  console.log(raw)  
+const PieChartData = ({ raw }) => {
+  console.log(raw)
   const data = Object.entries(raw[0]).map(([name, value]) => ({
-            name,
-            value,
-          }));
+    name,
+    value,
+  }));
   console.log(data.length)
   console.log(data)
   if (!data.length) return <p>Loading chart...</p>
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100}>
+    <ResponsiveContainer width="100%" height={300} >
+      <PieChart className="pie-chart-container">
+        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} innerRadius={70}>
           {data.map((entry, index) => (
             <Cell key={index} fill={colors[index % colors.length]} />
           ))}
         </Pie>
-        <Tooltip labelClassName=""/>
-        <Legend className="tooltip"/>
+        <Tooltip labelClassName="" />
+        <Legend className="tooltip" />
       </PieChart>
     </ResponsiveContainer>
   )
